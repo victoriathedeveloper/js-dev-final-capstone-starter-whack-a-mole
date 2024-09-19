f7ba5fcd80590e33c64b68a8ce8d79133465acc9
Victoria Holt <victoria.sholt@icloud.com>
2024-09-18 20:28:17 -0500

    activated randomInteger function

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
